f46accf178485e21c6e31ea1fcee29a496e4f86c
Emmanuel Hernandez <86961961+EmmaxPlay@users.noreply.github.com>
2022-05-16 01:03:01 -0500

    0.02
    Creando las primeras validaciones

---
+++ js/app.js
@@ -0,0 +1,26 @@
+//Variables y selectores
+const formulario = document.querySelector("#agregar-gasto")
+const gastoListado = document.querySelector("#gastos ul")
+
+
+//Eventos
+
+eventListeners()
+function eventListeners(){
+    document.addEventListener("DOMContentLoaded", preguntarPresupuesto);
+}
+
+
+//Clases
+
+
+
+//Funciones
+function preguntarPresupuesto(){
+    const presupuestoUsuario = (prompt("¿Cúal es tu presupuesto?"))
+    console.log(Number( presupuestoUsuario));
+
+    if(presupuestoUsuario === "" || presupuestoUsuario === null || presupuestoUsuario === undefined || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0){
+        window.location.reload()
+    }
+}
