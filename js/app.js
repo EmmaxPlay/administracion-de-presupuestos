dba241f98bb90f3b74b1aa960a029fdfc798b707
Emmanuel Hernandez <86961961+EmmaxPlay@users.noreply.github.com>
2022-05-16 01:49:46 -0500

    1.04
    Validando gastos

--- js/app.js
+++ js/app.js
@@ -7,6 +7,8 @@ const gastoListado = document.querySelector("#gastos ul");
 eventListeners();
 function eventListeners() {
   document.addEventListener("DOMContentLoaded", preguntarPresupuesto);
+
+  formulario.addEventListener("submit", agregarGasto);
 }
 
 //Clases
@@ -26,6 +28,26 @@ class UI {
     //Lo agregamos al html
     document.querySelector("#total").textContent = presupuesto;
     document.querySelector("#restante").textContent = restante;
+  }
+
+  imprimirAlerta(mensaje, tipo) {
+    //Crear el div con el mensaje
+    const divMensaje = document.createElement("div");
+    divMensaje.classList.add("text-center", "alert");
+    if (tipo == "error") {
+      divMensaje.classList.add("alert-danger");
+    } else {
+      divMensaje.classList.add("alert-success");
+    }
+
+    //Mensaje de error
+    divMensaje.textContent = mensaje;
+
+    //Insertamos en el html
+    document.querySelector(".primario").insertBefore(divMensaje, formulario);
+    setTimeout(() => {
+      divMensaje.remove();
+    }, 2000);
   }
 }
 
@@ -52,4 +74,22 @@ function preguntarPresupuesto() {
   console.log(presupuesto);
 
   ui.insertarPresupuesto(presupuesto);
+}
+
+//Agregamos gasto
+function agregarGasto(e) {
+  e.preventDefault();
+
+  //Leer los datos del formulario
+  const nombre = document.querySelector("#gasto").value;
+  const cantidad = document.querySelector("#cantidad").value;
+
+  //Validar
+  if (nombre === "" || cantidad === "") {
+    ui.imprimirAlerta("Ambos campos son obligatorios", "error");
+  } else if (cantidad <= 0 || isNaN(cantidad)) {
+    ui.imprimirAlerta("Cantidad no válida", "error");
+    return;
+  }
+  console.log("agregando gasto");
 }
